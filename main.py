5b1c174b5b21cde0147667db8fb9cb38fb396018
StefanyTorres <106988102+Stefany1501@users.noreply.github.com>
2025-02-23 22:22:02 -0300

    main att

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from routes import aeronave, cia
+from routes import aeronave, voo, cia
 
 # Inicializa o aplicativo FastAPI
 app = FastAPI()
 
 # Rotas para Endpoints
 app.include_router(aeronave.router)
+app.include_router(voo.router)
 app.include_router(cia.router)
